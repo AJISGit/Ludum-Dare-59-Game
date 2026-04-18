628c9afe595043012d6dbccad4507662622b0e95
AJISGit <spacedaj@protonmail.com>
2026-04-18 16:09:30 -0500

    So many changes, bu I mainly added different shooting modes for the enemy.
    I also added a new art for the enemy bullets, and I moved the health
    bar.

--- assets/scripts/enemy.gd
+++ assets/scripts/enemy.gd
@@ -1,9 +1,126 @@
 extends CharacterBody2D
 
-const BULLET_RESOURCE: PackedScene = preload("res://assets/scenes/bullet.tscn")
+const BULLET_RESOURCE: PackedScene = preload("res://assets/scenes/enemy_bullet_1.tscn")
 @onready var move_timer: Timer = $MoveTimer
 
 @export var health = 3
+
+enum shoot_phase {cshoot, aim, chaos}
+
+var phase: shoot_phase = randi_range(0, 2) as shoot_phase 
+var can_shoot: bool = true
+
+
+
+func switch_phase(current_phase: shoot_phase):
+	phase = randi_range(0, 2) as shoot_phase
+	if (phase == current_phase):
+		switch_phase(current_phase)
+	else:
+		can_shoot = false
+		$SwitchTimer.start()
+
+
+
+# Per phase data
+# Cshoot
+
+var bullet_dir: float = 0
+
+func cshoot_phase():
+
+	if bullet_dir >= 1080:
+		bullet_dir = 0
+		switch_phase(phase)
+		return
+
+	$ShootTimer.wait_time = 0.05
+	var bullet: Bullet = BULLET_RESOURCE.instantiate()
+	bullet.hit.connect(_on_bullet_hit)
+	bullet.position = position
+	bullet.speed = 40
+	bullet.rotation_degrees = bullet_dir
+	$"../".add_child(bullet)
+
+	bullet_dir += 10
+
+
+# Aim
+
+var aim_shot: int = 0
+
+func aim_phase():
+
+	if aim_shot > 75:
+		aim_shot = 0
+		switch_phase(phase)
+		return
+
+
+
+	$ShootTimer.wait_time = 0.35
+	var bullet: Bullet = BULLET_RESOURCE.instantiate()
+	bullet.hit.connect(_on_bullet_hit)
+	bullet.position = position
+	bullet.speed = 75
+	bullet.look_at(%Player.position) 
+	$"../".add_child(bullet)
+
+
+
+	var bullet2: Bullet = BULLET_RESOURCE.instantiate()
+	bullet2.hit.connect(_on_bullet_hit)
+	bullet2.position = position
+	bullet2.speed = 75
+	bullet2.look_at(%Player.position) 
+	bullet2.rotation_degrees += 30
+	$"../".add_child(bullet2)
+
+
+
+	var bullet3: Bullet = BULLET_RESOURCE.instantiate()
+	bullet3.hit.connect(_on_bullet_hit)
+	bullet3.position = position
+	bullet3.speed = 75
+	bullet3.look_at(%Player.position) 
+	bullet3.rotation_degrees -= 30
+	$"../".add_child(bullet3)
+
+
+	aim_shot += 1
+
+
+# Chaos
+
+var chaos_dir1: float = 0
+var chaos_dir2: float = 1800
+
+func chaos_phase():
+
+	if chaos_dir1 > 1800:
+		chaos_dir1 = 0
+		chaos_dir2 = 1800
+		switch_phase(phase)
+		return
+
+	$ShootTimer.wait_time = 0.05
+	var bullet: Bullet = BULLET_RESOURCE.instantiate()
+	bullet.hit.connect(_on_bullet_hit)
+	bullet.position = position
+	bullet.speed = 30 
+	bullet.rotation_degrees = chaos_dir1
+	$"../".add_child(bullet)
+
+	var bullet2: Bullet = BULLET_RESOURCE.instantiate()
+	bullet2.hit.connect(_on_bullet_hit)
+	bullet2.position = position
+	bullet2.speed = 15 
+	bullet2.rotation_degrees = chaos_dir2
+	$"../".add_child(bullet2)
+
+	chaos_dir1 += 10
+	chaos_dir2 -= 15
+
 
 
 func _on_move_timer_timeout():
@@ -37,18 +154,27 @@ func _on_bullet_hit(body: Node2D):
 	if (body == %Player):
 		%Player.health -= 1
 
-func _on_shoot_timer_timeout():
 
-	var bullet: Bullet = BULLET_RESOURCE.instantiate()
-	bullet.hit.connect(_on_bullet_hit)
-	bullet.position = position
-	bullet.speed = 100
-	bullet.look_at(%Player.position)
-	$"../".add_child(bullet)
+func _on_switch_timer_timeout():
+	can_shoot = true
+
+
+
+func _on_shoot_timer_timeout():
+	
+	if !can_shoot: return
+
+	if phase == shoot_phase.cshoot:
+		cshoot_phase()
+	elif phase == shoot_phase.aim:
+		aim_phase()
+	elif phase == shoot_phase.chaos:
+		chaos_phase()
 
 
 func _ready() -> void:
 	$ShootTimer.timeout.connect(_on_shoot_timer_timeout)
+	$SwitchTimer.timeout.connect(_on_switch_timer_timeout)
 
 	move_timer.timeout.connect(_on_move_timer_timeout)
 	move_timer.start()
